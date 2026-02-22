09e495c04bd313d4b8ad02a16bbe9f46fe6708cb
Dmutro <dtyslyak@gmail.com>
2026-02-22 16:49:07 +0200

    Add migrations

--- Contact Manager Application/Data/AppDbContext.cs
+++ Contact Manager Application/Data/AppDbContext.cs
@@ -8,5 +8,11 @@ namespace Contact_Manager_Application.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Contact> Contacts { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>()
+                .Property(c => c.Salary)
+                .HasColumnType("decimal(18,4)"); 
+        }
     }
 }
